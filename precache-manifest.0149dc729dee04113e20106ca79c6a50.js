1b7106744934b04002333a043aba6a7519e308f5
Rei Krom <mr.mygo@gmail.com>
2020-09-14 21:10:29 +0100

    Updates

---
+++ precache-manifest.0149dc729dee04113e20106ca79c6a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f25a3cbf7ffadd80bae7c2e4e23ff6f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "b2dc7a8d41640c49a94a",
+    "url": "/react-todo-app/static/css/main.f7e635ac.chunk.css"
+  },
+  {
+    "revision": "a2707d70ef56b795439c",
+    "url": "/react-todo-app/static/js/2.d0581805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.d0581805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2dc7a8d41640c49a94a",
+    "url": "/react-todo-app/static/js/main.7f35d51d.chunk.js"
+  },
+  {
+    "revision": "c38f3f092b6a442ea04a",
+    "url": "/react-todo-app/static/js/runtime-main.548fee87.js"
+  }
+]);
